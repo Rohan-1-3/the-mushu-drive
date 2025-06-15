7c2935489fc1c82d4fa6748ad53d03c0cbe795bb
Rohan-1-3 <rohanshrestha130@gmail.com>
2025-06-15 18:46:55 +0545

    feat: setup controller for user

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,11 @@
 import { body } from "express-validator"
 import { validateRequest } from "../configs/validateRequest"
+import { hash } from "bcryptjs"
+import { PrismaClient } from "@prisma/client/extension"
+import expressAsyncHandler from "express-async-handler";
+import {v4 as uuid} from "uuid"
 
+const prisma = new PrismaClient();
 
 const validateUserName = [
     body('firstname').trim().notEmpty().withMessage("First name cannot be empty.").bail()
@@ -20,9 +25,105 @@ const validateUserCredentials = [
     .escape()
 ]
 
+function getChangedFields(existing, incoming) {
+  const changes = {};
+  for (const key in incoming) {
+    if (incoming[key] !== existing[key]) {
+      changes[key] = incoming[key];
+    }
+  }
+  return changes;
+}
+
 export const registerUser= [
     validateUserName, validateUserCredentials,
-    validateRequest,
+    validateRequest, expressAsyncHandler(async (req, res)=>{
+        const {firstname, lastname, username, password } = req.body
+        const hashedPass = await hash(password, 16);
+        const createdUser= await prisma.user.create({
+            data:{
+                id: uuid(),
+                firstname: firstname,
+                lastname: lastname,
+                username: username,
+                password: hashedPass
+            }
+        })
+        res.status(200).json({createdUser})
+    })
 ]
 
-export const loginUser = [validateUserCredentials, validateRequest]
+export const loginUser = [validateUserCredentials, validateRequest,
+    expressAsyncHandler(async (req, res, next)=>{
+        passport.authenticate('local', (err, user, info) => {
+            if (err) return next(err);
+            if (!user) return res.status(401).json({err: [info]});
+            
+            req.logIn(user, (err) => {
+                if (err) return next(err);
+                res.status(200).json({
+                    message: 'Login successful',
+                    user
+                });
+            });
+        })(req, res, next);
+    })
+]
+
+export const logoutUser = expressAsyncHandler((req, res, next)=>{
+    req.logout(function (err) {
+        if (err) {
+            return res.status(500).json({ message: 'Logout failed', error: err.message });
+        }
+
+        req.session.destroy(() => {
+            res.clearCookie("connect.sid");
+            res.status(200).json({ message: 'Logout successful' });
+        });
+    });
+})
+
+export const authenticateUser = (req, res, next)=>{
+    if(req.isAuthenticated()){
+        res.status(400).json({
+            message: "User authenticated"
+        })
+    }
+    res.status(400).json({
+        message: "User is not authenticated"
+    })
+}
+
+export const updateUser = [
+    validateUserName, validateUserCredentials, validateRequest,
+    expressAsyncHandler(async (req, res)=>{
+        const existingUser = await prisma.user.findUnique({
+            where: { id: req.body.id }
+        });
+        if(!existingUser){
+            res.status(400).json({
+                err: {
+                    msg: "No such user exists."
+                }
+            })
+        }
+        const changes = getChangedFields(existingUser, req.body)
+        let updatedUser;
+        if (Object.keys(changes).length > 0) {
+            updatedUser = await prisma.user.update({
+                where: { id: userId },
+                data: changes
+            });
+        }
+        res.status(200).json({updatedUser})
+    }) 
+]
+
+export const deleteUser = expressAsyncHandler(async (req, res)=>{
+    await prisma.user.delete({
+        where:{
+            id: req.body.id
+        }
+    })
+    res.status(200).json({deleted: true})
+})
